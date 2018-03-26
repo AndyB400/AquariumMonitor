8e14cca1437f67d08613424cbb2e7858d413200c
Andy Bradford <andy.brad@gmail.com>
2018-03-26 11:30:00 +0100

    Moved Models and Model.Tests to .Net Core

--- Models/Validation/EntityValidator.cs
+++ Models/Validation/EntityValidator.cs
@@ -10,31 +10,12 @@ namespace AquariumMonitor.Models.Validation
     {
         public static List<ValidationResult> Validate(object instance, bool validateAllProperties = true)
         {
-            RegisterMetadataClass(instance);
-
             var validationContext = new ValidationContext(instance, null, null);
             var validationResults = new List<ValidationResult>();
 
             Validator.TryValidateObject(instance, validationContext, validationResults, validateAllProperties);
 
             return validationResults;
-        }
-
-        private static void RegisterMetadataClass(object instance)
-        {
-            var modelType = instance.GetType();
-            var metadataType = GetMetadataType(modelType);
-
-            if (metadataType != null)
-            {
-                TypeDescriptor.AddProviderTransparent(new AssociatedMetadataTypeTypeDescriptionProvider(modelType, metadataType), modelType);
-            }
-        }
-
-        private static Type GetMetadataType(Type type)
-        {
-            var attribute = (MetadataTypeAttribute)type.GetCustomAttributes(typeof(MetadataTypeAttribute), true).FirstOrDefault();
-            return attribute == null ? null : attribute.MetadataClassType;
         }
     }
 }
